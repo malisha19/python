f65287efffda30b5a1e75c01993efc293856b152
jonnygong <850954932@qq.com>
2020-01-29 21:16:19 +0800

    python03 第三节学习笔记

---
+++ three.py
@@ -0,0 +1,23 @@
+# a = 10
+# b = 3
+# a += b
+# a *= a + 2
+# print(a)
+# flag0 = 1 == 1
+# flag1 = 3 > 2
+# flag2 = 2 < 1
+# flag3 = flag1 and flag2
+# flag4 = flag1 or flag2
+# flag5 = not(1!=2)
+# print('flag0 = ',flag0)
+# print('flag1 = ',flag1)
+# print('flag2 = ',flag2)
+# print('flag3 = ',flag3)
+# print('flag4 = ',flag4)
+# print('flag5 = ',flag5)
+# print(flag1 is True )
+# print(flag2 is not False)
+# f = float (input('请输入华氏温度：'))
+# c = (f - 32)/1.8
+# print('%.1f华氏度 = %.1f摄氏度'%(f,c))
+
